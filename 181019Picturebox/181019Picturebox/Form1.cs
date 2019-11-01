e5b14e70a3ed058372d99ac66359ae430b0f7c49
alphamonstaTheDEV <46228825+alphamonstaTheDEV@users.noreply.github.com>
2019-11-01 17:12:19 +0300

    PictureBOX
    PictureBOX

---
+++ 181019Picturebox/181019Picturebox/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _181019Picturebox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDegistir_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog ofd = new OpenFileDialog();
+
+            //DialogResult sonuc = ofd.ShowDialog();
+
+            //if (sonuc== DialogResult.OK)
+            //{
+            //    pictureBox1.ImageLocation = ofd.FileName;
+            //}
+            //pictureBox1.Image = Properties.Resources.ataturk;
+            pictureBox1.ImageLocation = "ataturk.jpg";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
